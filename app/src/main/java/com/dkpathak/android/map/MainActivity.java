abe0e99bf6e62f6546e52010f94169c4767cdb0c
dkpathak <ismd.dk@gmail.com>
2016-03-06 16:10:48 +0530

    location added

--- app/src/main/java/com/dkpathak/android/map/MainActivity.java
+++ app/src/main/java/com/dkpathak/android/map/MainActivity.java
@@ -3,6 +3,7 @@ package com.dkpathak.android.map;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,15 +14,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double lon;
+    double lat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GPSTracker gpsTracker = new GPSTracker(this);
+
+        if(gpsTracker.getIsGPSTrackingEnabled()){
+            lon = gpsTracker.getLongitude();
+            lat= gpsTracker.getLatitude();
+        }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -38,9 +49,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng location = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(location).title("Your Location"));
+        CameraUpdate cm = CameraUpdateFactory.newLatLngZoom(location,15);
+        mMap.moveCamera(cm);
+        mMap.animateCamera(cm, 2000,null);
     }
 }
